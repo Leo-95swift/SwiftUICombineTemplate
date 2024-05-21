1089a455d78a651a94a29010fb16dde8996ad917
xach55 <xach55@gmail.com>
2024-05-21 19:49:40 +0400

    initial commit

---
+++ SwiftUICombineTemplate/SwiftUICombineTemplate/SwiftUICombineTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineTemplateApp.swift
+//  SwiftUICombineTemplate
+//
+//  Created by Levon Shaxbazyan on 21.05.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
