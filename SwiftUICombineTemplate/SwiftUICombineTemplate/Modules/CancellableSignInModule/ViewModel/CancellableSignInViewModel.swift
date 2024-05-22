b2a6ba10744866ee4ee87fd82240808fc7f60015
xach55 <xach55@gmail.com>
2024-05-22 10:17:49 +0400

    закончил задачу 3 на SwiftUI

---
+++ SwiftUICombineTemplate/SwiftUICombineTemplate/Modules/CancellableSignInModule/ViewModel/CancellableSignInViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  CancellableSignInViewModel.swift
+//  SwiftUICombineTemplate
+//
+//  Created by Levon Shaxbazyan on 21.05.24.
+//
+
+import SwiftUI
+import Combine
+
+/// Для бизнес логики экрана регистрации
+final class CancellableSignInViewModel: ObservableObject {
+    
+    @Published var email = ""
+    @Published var password = ""
+    @Published var emailValidation = ""
+    @Published var passwordValidation = ""
+    @Published var validationStatus = ""
+
+    var cancellable: AnyCancellable?
+
+    var isSignInButtonEnabled: Bool {
+        return !email.isEmpty && !password.isEmpty
+    }
+    
+    init() {
+        cancellable = $email
+            .map { [unowned self] value -> String in
+                validationStatus = "Запрос на вход"
+                return value
+            }
+            .delay(for: 5, scheduler: DispatchQueue.main)
+            .sink { [unowned self] value in
+                self.validationStatus = "Успешный вход"
+            }
+        
+    }
+    
+    func cuncel() {
+        validationStatus = "Операция отменена"
+        cancellable?.cancel()
+        cancellable = nil
+    }
+}
+
